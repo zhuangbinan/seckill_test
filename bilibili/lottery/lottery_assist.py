3864c60d49a2c1bfce431094669f824129282854
zhuangbinan <296700879@qq.com>
2022-09-28 00:42:50 +0800

    走完了一遍流程,任需要修改

--- bilibili/lottery/lottery_assist.py
+++ bilibili/lottery/lottery_assist.py
@@ -154,7 +154,9 @@ class LotteryAssist:
                                             a_tag = driver.find_element(By.XPATH, avatarXpath)
                                             print(a_tag.get_attribute('href')) # https://space.bilibili.com/3766866
                                             space_url = a_tag.get_attribute('href')
+                                            space_url = 'https://space.bilibili.com/178690106'
                                             uid = space_url.split('com/')[1]
+                                            uid = '178690106'
                                             print(uid) # uid
                                             a_tag.click()
                                             time.sleep(3)
@@ -179,7 +181,8 @@ class LotteryAssist:
                                             res = requests.get(get_dynamic_id_url)
                                             loaded = json.loads(res.text)
                                             dynamic_id = loaded.get('data').get('items')[0].get('id_str')
-                                            if loaded.get('data').get('items')[0].get('modules').get('module_dynamic').get('desc').get('rich_text_nodes')[0].get('text') == '​互动抽奖':
+                                            hdcj_str = loaded.get('data').get('items')[0].get('modules').get('module_dynamic').get('desc').get('rich_text_nodes')[0].get('text')
+                                            if hdcj_str.endswith('互动抽奖'):
                                                 print('是互动抽奖')
                                                 # 判断是否已开奖
                                                 # https://api.vc.bilibili.com/lottery_svr/v1/lottery_svr/lottery_notice?dynamic_id=700543869440229449
@@ -190,6 +193,34 @@ class LotteryAssist:
                                                     print('还没开奖')
                                                     # 给它关注,转发,评论套餐
                                                     # 关注是必须的
+                                                    # 判断是否关注了
+                                                    # className = h-f-icon
+                                                    gz_btn_xpath = '//*[@id="app"]/div[1]/div[1]/div[2]/div[4]/span'
+                                                    if LotteryAssist.iselementByXpath(driver, gz_btn_xpath):
+                                                        print('没关注呢')
+                                                        gz_btn = driver.find_element(By.XPATH, gz_btn_xpath)
+                                                        gz_btn.click()
+                                                        time.sleep(1)
+                                                        # 关注完了要转发 //*[@id="page-dynamic"]/div[1]/div/div[1]/div[1]/div/div[2]/div[4]/div[1]/div
+                                                        zf_xpath = '//*[@id="page-dynamic"]/div[1]/div/div[1]/div[1]/div/div[2]/div[4]/div[1]/div'
+                                                        if LotteryAssist.iselementByXpath(driver, zf_xpath):
+                                                            print('找到转发按钮')
+                                                            zf_btn = driver.find_element(By.XPATH, zf_xpath)
+                                                            zf_btn.click()
+                                                            time.sleep(2)
+                                                            zf_input_div_xpath = '//*[@id="page-dynamic"]/div[1]/div/div[1]/div[1]/div/div[3]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div[1]/div'
+                                                            if LotteryAssist.iselementByXpath(driver, zf_input_div_xpath):
+                                                                print('找到转发时填入内容框')
+                                                                zf_input_div = driver.find_element(By.XPATH, zf_input_div_xpath)
+                                                                print('准备填入内容')
+                                                                zf_input_div.send_keys('抽我抽我请抽我')
+                                                                zf_submit_xpath = '//*[@id="page-dynamic"]/div[1]/div/div[1]/div[1]/div/div[3]/div[1]/div[1]/div/div[2]/div[2]/div[2]/button'
+                                                                if LotteryAssist.iselementByXpath(driver, zf_submit_xpath):
+                                                                    print('找到了转发按钮')
+                                                                    zf_submit_btn = driver.find_element(By.XPATH, zf_submit_xpath)
+                                                                    zf_submit_btn.click()
+                                                                    print('点击了转发按钮')
+
 
 
                                         else:
