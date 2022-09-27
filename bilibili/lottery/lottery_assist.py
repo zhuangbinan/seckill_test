2f98df9003fd7b13c71d69470a05e652d17b5477
zhuangbinan <zhuangbinan1024@foxmail.com>
2022-09-27 18:24:52 +0800

    lottery asssit for bilibili not ready

---
+++ bilibili/lottery/lottery_assist.py
@@ -0,0 +1,230 @@
+import requests
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+import time
+import json
+
+from selenium.webdriver.chrome.service import Service
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
+}
+
+
+
+class LotteryAssist:
+
+    def iselementById(browser, id):
+        """
+        基本实现判断元素是否存在
+        :param browser: 浏览器对象
+        :param id: id
+        :return: 是否存在
+        """
+        try:
+            browser.find_element(By.ID, id)
+            return True
+        except:
+            return False
+
+    def iselementByXpath(browser, xpaths):
+        """
+        基本实现判断元素是否存在
+        :param browser: 浏览器对象
+        :param xpaths: xpaths表达式
+        :return: 是否存在
+        """
+        try:
+            browser.find_element(By.XPATH, xpaths)
+            print('is a ele')
+            return True
+        except:
+            print('not a ele')
+            return False
+
+    def iselementByClassName(browser, class_name):
+        """
+        基本实现判断元素是否存在
+        :param browser: 浏览器对象
+        :param class_name: class_name
+        :return: 是否存在
+        """
+        try:
+            browser.find_element(By.CLASS_NAME, class_name)
+            return True
+        except:
+            return False
+
+    def openChromeToBilibili(self):
+        # 使用开发者模式
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+        # 禁用 Blink 运行时的某些功能
+        options.add_argument(" --disable-blink-features=AutomationControlled")
+
+        # 创建 driver 之后将 window.navigator.webdriver 改成 undefined
+        # driver = webdriver.Chrome(options=options)
+
+        s = Service(r'C:\Program Files\Google\Chrome\Application\chromedriver')
+        driver = webdriver.Chrome(options=options, service=s)
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+        })
+
+        # uid
+        uid = None
+        bilibiliUrl = 'https://www.bilibili.com'
+        # 喵喵折APP 有个抽完的
+        # 科技美学 有还没抽完的
+        driver.get(bilibiliUrl)
+        time.sleep(3)
+
+
+        if LotteryAssist.iselementByClassName(driver, 'header-login-entry'):
+            print('点击登录按钮', flush=True)
+            driver.find_element(By.CLASS_NAME, 'header-login-entry').click()
+            while (1 == 1):
+                if LotteryAssist.iselementByClassName(driver, 'header-avatar-wrap'):
+                    print("已经登录")
+                    break
+                else:
+                    print("没登录")
+                time.sleep(5)
+
+            time.sleep(3)
+            # print("假如登录了")
+
+            if LotteryAssist.iselementByClassName(driver, 'nav-search-input'):
+                print("找到搜索框")
+                search_input = driver.find_element(By.CLASS_NAME, 'nav-search-input')
+                search_input.send_keys('科技美学')
+                if LotteryAssist.iselementByClassName(driver, 'nav-search-btn'):
+                    print("点击搜索按钮")
+                    search_btn = driver.find_element(By.CLASS_NAME, 'nav-search-btn')
+                    search_btn.click()
+                    types = driver.find_elements(By.CLASS_NAME, 'vui_tabs--nav-text')
+                    print(types)
+                    time.sleep(3)
+
+                    print("准备XPATH")
+                    # 应该要在新的窗口搜索
+                    driver.switch_to.window(driver.window_handles[1])
+                    if LotteryAssist.iselementByXpath(driver, '//*[@id="i_cecream"]/div/div[2]/div[1]/div[2]/div/nav/ul/li[8]/span/span[1]'):
+                        type_user = driver.find_element(By.XPATH, '//*[@id="i_cecream"]/div/div[2]/div[1]/div[2]/div/nav/ul/li[8]/span/span[1]')
+                        print('用户那一栏分类')
+                        time.sleep(3)
+                        print(type_user.text)
+                        if type_user.text == '用户':
+                            print('是用户的span')
+                            print(driver.current_url)
+                            # https://search.bilibili.com/all?keyword
+                            # &order=fans
+
+                            current_url = driver.current_url
+                            pre_url = 'https://search.bilibili.com/upuser?keyword'
+                            end_url = '&order=fans'
+                            final_url = current_url.replace('https://search.bilibili.com/all?keyword', pre_url) + end_url
+
+                            # type_user.click()
+                            print(final_url)
+                            driver.get(final_url)
+                            time.sleep(5)
+                            # 394.2万粉丝
+                            # 1386粉丝
+                            # //*[@id="i_cecream"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div/div/p
+                            if LotteryAssist.iselementByXpath(driver, '//*[@id="i_cecream"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div/div/p'):
+                                print('查到了粉丝数')
+                                fans_title = driver.find_element(By.XPATH, '//*[@id="i_cecream"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div/div/p')
+                                print(fans_title.text)
+                                fans_title_str = fans_title.text
+                                split_array = fans_title_str.split('粉丝')
+                                print(split_array[0])  # 394.2万
+                                if split_array[0].find('万') != -1:
+                                    fans_num = split_array[0].split('万')
+                                    if float(fans_num[0]) > 1:
+                                        # 大于1W的 可以看有没有抽奖的
+                                        print('粉丝数大于1W的')
+                                        # //*[@id="i_cecream"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div/div/div/button  关注按钮
+                                        time.sleep(3)
+                                        # //*[@id="i_cecream"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div/a 头像a标签
+                                        avatarXpath = '//*[@id="i_cecream"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div/a'
+                                        if LotteryAssist.iselementByXpath(driver, avatarXpath):
+                                            a_tag = driver.find_element(By.XPATH, avatarXpath)
+                                            print(a_tag.get_attribute('href')) # https://space.bilibili.com/3766866
+                                            space_url = a_tag.get_attribute('href')
+                                            uid = space_url.split('com/')[1]
+                                            print(uid) # uid
+                                            a_tag.click()
+                                            time.sleep(3)
+                                            dyn = '/dynamic'
+                                            dyn_url = space_url + dyn
+                                            print(dyn_url)  # https://space.bilibili.com/3766866/dynamic
+                                            driver.switch_to.window(driver.window_handles[2])
+                                            driver.close()
+                                            driver.switch_to.window(driver.window_handles[1])
+                                            time.sleep(1)
+                                            # driver.get(dyn_url)
+                                            newWindowUrl = "window.open('" + dyn_url + "')"
+                                            driver.execute_script(newWindowUrl)
+                                            print('打开了动态页面')
+                                            driver.switch_to.window(driver.window_handles[2])
+                                            time.sleep(3)
+
+                                            print('只找置顶是互动抽奖的')
+                                            # 按uid(host_mid)获取dynamic_id(id_str)
+                                            # https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&timezone_offset=-480&host_mid=7207905
+                                            get_dynamic_id_url = 'https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&timezone_offset=-480&host_mid=' + uid
+                                            res = requests.get(get_dynamic_id_url)
+                                            loaded = json.loads(res.text)
+                                            dynamic_id = loaded.get('data').get('items')[0].get('id_str')
+                                            if loaded.get('data').get('items')[0].get('modules').get('module_dynamic').get('desc').get('rich_text_nodes')[0].get('text') == '​互动抽奖':
+                                                print('是互动抽奖')
+                                                # 判断是否已开奖
+                                                # https://api.vc.bilibili.com/lottery_svr/v1/lottery_svr/lottery_notice?dynamic_id=700543869440229449
+                                                lottery_result_url = 'https://api.vc.bilibili.com/lottery_svr/v1/lottery_svr/lottery_notice?dynamic_id=' + dynamic_id
+                                                response = requests.get(lottery_result_url)
+                                                lottery_result_loaded = json.loads(response.text)
+                                                if lottery_result_loaded.get('data').get('lottery_result') is None:
+                                                    print('还没开奖')
+                                                    # 给它关注,转发,评论套餐
+                                                    # 关注是必须的
+
+
+                                        else:
+                                            print(' no ')
+
+
+                                        time.sleep(101)
+                                time.sleep(10)
+                            else:
+                                print('没查到粉丝数')
+                            time.sleep(10)
+                        else:
+                            print('不是用户的span')
+
+                        time.sleep(10)
+
+                    else:
+                        print('没找到用户那一栏分类')
+                        time.sleep(10)
+
+
+                    time.sleep(10)
+                else:
+                    print("没找到搜索按钮")
+                    time.sleep(10)
+                time.sleep(100)
+            else:
+                print("没找到搜索框")
+
+            time.sleep(100)
+        else:
+            print("没找到登录按钮")
+            time.sleep(100)
+
+
+
+a = LotteryAssist()
+a.openChromeToBilibili()
